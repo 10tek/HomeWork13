32dfc18c0b298c5d7b84d7de6d3b8c6540dfa5d9
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-16 20:00:49 +0600

    Выводится на экран позиция и значение элемента, являющегося вторым максимальным значением в коллекции и сумма элементов на четных позициях.

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var listOfNumbers = new List<int>();
+            var sum = 0;
+            var sizeOfList = 10;
+            for (var i = 0; i < sizeOfList; i++)
+            {
+                listOfNumbers.Add(rand.Next(0, 100));
+                if (i % 2 == 0)
+                {
+                    sum += listOfNumbers[i];
+                }
+            }
+            int penultPos = listOfNumbers.Count - 2;
+            listOfNumbers.Sort();
+            Console.WriteLine($"Index: {penultPos}  Value: {listOfNumbers[penultPos]}");
+            Console.WriteLine($"Sum of elements in even positions: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
